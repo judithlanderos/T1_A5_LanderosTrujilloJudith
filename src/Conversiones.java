0fbdb4541334a7f9826c40b1e300f5e052bc429b
Judith Landeros <judith.lt@tecjerez.edu.mx>
2024-02-29 16:55:29 -0600

    Primer commit

---
+++ src/Conversiones.java
@@ -0,0 +1,16 @@
+
+public class Conversiones {
+	
+		    public static double celsiusToFahrenheit(double celsius) {
+		        return (celsius * 9 / 5) + 32;
+		    }
+		    
+		    public static double celsiusToKelvin(double celsius) {
+		        return celsius + 273.15;
+		    }
+		    
+		    public static double celsiusToRankine(double celsius) {
+		        return (celsius + 273.15) * 9 / 5;
+		    }
+
+	}
---
+++ src/VentanaPrincipal.java
@@ -0,0 +1,83 @@
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+
+public class VentanaPrincipal extends JFrame implements ActionListener {
+    
+    private JLabel txtConvertir, txtA, txtIgual;
+    private JTextField cajaUno, cajaDos;
+    private JComboBox<String> comboGrados;
+    
+    public VentanaPrincipal() {
+        getContentPane().setLayout(new FlowLayout());
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(300, 300);
+        setLocationRelativeTo(null);
+        setTitle("Conversiones");
+        setVisible(true);
+        
+        txtConvertir = new JLabel();
+        txtConvertir.setText("Convertir");
+        add(txtConvertir);
+        
+        cajaUno = new JTextField("70", 10);
+        add(cajaUno);
+        
+        comboGrados = new JComboBox<String>();
+        comboGrados.addItem("CELSIUS");
+        comboGrados.addItem("FAHRENHEIT");
+        comboGrados.addItem("KELVIN");
+        comboGrados.addItem("RANKINE");
+        comboGrados.addActionListener(this); 
+        add(comboGrados);
+        
+        txtA = new JLabel();
+        txtA.setText("A:");
+        add(txtA);
+        
+        txtIgual = new JLabel();
+        txtIgual.setText("=");
+        add(txtIgual);
+        
+        cajaDos = new JTextField(10);
+        cajaDos.setEditable(false); 
+        add(cajaDos);
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == comboGrados) { 
+            String seleccion = (String) comboGrados.getSelectedItem(); 
+            double valor = Double.parseDouble(cajaUno.getText()); 
+            
+            switch (seleccion) {
+                case "CELSIUS":
+                    cajaDos.setText(String.valueOf(valor)); 
+                    break;
+                case "FAHRENHEIT":
+                    cajaDos.setText(String.valueOf(Conversiones.celsiusToFahrenheit(valor)));
+                    break;
+                case "KELVIN":
+                    cajaDos.setText(String.valueOf(Conversiones.celsiusToKelvin(valor)));
+                    break;
+                case "RANKINE":
+                    cajaDos.setText(String.valueOf(Conversiones.celsiusToRankine(valor)));
+                    break;
+            }
+        }
+    }
+    
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new VentanaPrincipal();
+            }
+        });
+    }
+}
